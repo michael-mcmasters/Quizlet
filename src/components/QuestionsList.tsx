56805e2f914b315c3427b3a636dfffe83c856a9a
Michael McMasters <jmcmasters411@gmail.com>
2022-06-12 00:35:29 -0400

    Provide UUID to objects when one isn't provided (to make it easier to test)

--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -1,7 +1,10 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import questionsJson from '../resources/questions.json'
+// import questionsJson from '../resources/aws-questions.json'
 import Question from './Question';
 import styles from "../styles/QuestionList.module.css"
+import { v4 as uuidv4 } from 'uuid';
+
 
 interface Props {
   
@@ -9,17 +12,22 @@ interface Props {
 
 function QuestionsList(props: Props) {
   
+  const [questions, setQuestions] = useState<any[]>(questionsJson.questions.map(q => {
+    const uuid = (q.uuid == undefined || q.uuid == 0) ? uuidv4() : q.uuid;
+    return { ...q, uuid };
+  } ));
+  
   const answers: string[] = useMemo(() => questionsJson.questions.map(q => q.answer), [questionsJson]);
   
   return (
     <div className={styles.container}>
-      {questionsJson.questions.map(q => (
-        <Question
-          key={q.uuid}
-          question={q.question}
-          correctAnswer={q.answer}
-          allAnswers={answers}
-        />
+      {questions.map(q => (
+          <Question
+            key={q.uuid}
+            question={q.question}
+            correctAnswer={q.answer}
+            allAnswers={answers}
+          />
       ))}
     </div>
   );
