e99b1e59491ca2a41583dad227694ba9a8bf55e3
Michael McMasters <jmcmasters411@gmail.com>
2022-07-16 14:35:04 -0400

    Fix warning message

--- src/components/MultiQuestion.tsx
+++ src/components/MultiQuestion.tsx
@@ -26,7 +26,7 @@ function MultiQuestion(props: Props) {
         usedIndexes.push(wrongAnswerIndex);
         
         if (usedIndexes.length >= 3) {
-          console.warn("There are more than 3 wrong answers provided for this question. Ignoring additional warnings as one slot must have correct answer.")
+          console.warn("There are more than 3 wrong answers provided for this question. Ignoring additional answers as one slot must have correct answer.")
           break;
         }
       }
